b97d3f415a9e0996527981696c15a04973b9d3e7
AndreaBV00 <azulaitami@gmail.com>
2024-09-09 21:52:56 -0300

    app

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=50)
+    camada = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre
